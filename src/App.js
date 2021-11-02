740c47d7ad2522fc81365ba4a84d12e7884212bc
thiago-costa-dev <costathiagocontat@gmail.com>
2021-11-02 06:57:46 -0300

    gitignore is now working

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Stopwatch from './components/Stopwatch';
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return <Stopwatch />;
+    }
+}
